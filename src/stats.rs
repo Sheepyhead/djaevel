59f5aebcc498a8da566859738a6ecf7350105caa
Sheepyhead <trojes@tuta.io>
2022-10-24 13:12:29 +0200

    Added death system

--- src/stats.rs
+++ src/stats.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
 
+use crate::player::Player;
+
 pub struct Stats;
 
 impl Plugin for Stats {
     fn build(&self, app: &mut App) {
         app.add_event::<DamageInflicted>()
-            .add_system(inflict_damage);
+            .add_event::<Death>()
+            .add_system(inflict_damage)
+            .add_system(death);
     }
 }
 
@@ -44,16 +48,33 @@ pub struct DamageInflicted {
     pub amount: u32,
 }
 
-fn inflict_damage(mut events: EventReader<DamageInflicted>, mut targets: Query<&mut HitPoints>) {
-    for DamageInflicted { target, amount } in events.iter() {
+fn inflict_damage(
+    mut reader: EventReader<DamageInflicted>,
+    mut writer: EventWriter<Death>,
+    mut targets: Query<&mut HitPoints>,
+) {
+    for DamageInflicted { target, amount } in reader.iter() {
         if let Ok(mut hit_points) = targets.get_mut(*target) {
             if hit_points.dead() {
                 continue;
             }
             info!("Target {target:?} took {amount} damage!");
             if hit_points.subtract(*amount) {
-                info!("Target is dead!");
+                writer.send(Death(*target));
             }
+        }
+    }
+}
+
+pub struct Death(Entity);
+
+fn death(mut commands: Commands, mut reader: EventReader<Death>, player: Query<(), With<Player>>) {
+    for Death(target) in reader.iter() {
+        info!("Entity {target:?} has died");
+        if player.contains(*target) {
+            panic!("Player has died");
+        } else {
+            commands.entity(*target).despawn_recursive();
         }
     }
 }
